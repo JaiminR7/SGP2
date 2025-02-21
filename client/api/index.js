9822b53da5d87e179ce06c0c8bcd862ecaa4fdfe
JaiminR7 <173jaiminradia@gmail.com>
2025-02-22 02:14:40 +0530

    mongodb made

--- client/api/index.js
+++ client/api/index.js
@@ -1,13 +1,24 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const mongoose=require("mongoose");
+require('dotenv').config();
+app.use(express.json());
 app.use(cors({
     credentials:true,
     origin:'http://localhost:5173/',
 }));
 
+
+mongoose.connect(process.env.MONGO_URL);
 app.get('/test',(req , res)=>{
     res.json('test ok');
 });
+app.post('/register',(req,res)=>{
+    const {name,email,password}=req.body;
+    res.json({name,email,password});
+
+})
 
 app.listen(4000);
+//4rnSjU3xjQOjyNld
